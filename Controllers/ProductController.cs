1f1095ac63198a53c25e5709832589d3d0074bdc
Ashish Jain <arjain18@gmail.com>
2024-08-18 17:43:57 +0530

    Unable to add migration

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkWithMySQL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkWithMySQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
